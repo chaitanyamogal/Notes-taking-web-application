79bf4364bceba24866162aef4a424ba917964c80
chaitanyamogal <49728920+chaitanyamogal@users.noreply.github.com>
2020-02-15 16:52:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-console.log("hello world");
 
 // Event listener .....if the user adds the note to the local storage
 
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  };
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   console.log(notesObj);
   showNotes();
 });
@@ -31,8 +36,8 @@ function showNotes() {
     html += `
         <div class="notesCard my-2 mx-3 card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Note ${index + 1}</h5>
-          <p class="card-text">${element}</p>
+          <h5 class="card-title"> ${element.title}</h5>
+          <p class="card-text">${element.text}</p>
           <button id="${index}" onClick ="deleteNote(this.id)"class="btn btn-primary">Delete Note</button>
         </div>
         </div>
